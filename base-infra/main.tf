bbcb900dfb6eddfb81ddc8eaf412a7abf47b5f7f
Khant <apple@192.168.1.106>
2024-05-04 01:33:32 +0700

    fix error

--- base-infra/main.tf
+++ base-infra/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source       = "../../modules/vpc"
+  source       = "../modules/vpc"
   project_name = var.project_name
   vpc_name     = var.vpc_name
   environment  = var.environment
