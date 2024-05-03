e3b6b15e1e0d6ce987390ead1ed5a8786a8967c6
Khant <apple@192.168.1.106>
2024-05-04 01:30:35 +0700

    update workflow

--- base-infra/prod/main.tf
+++
@@ -1,6 +0,0 @@
-module "vpc" {
-  source       = "../../modules/vpc"
-  project_name = var.project_name
-  vpc_name     = var.vpc_name
-  environment  = var.environment
-}
--- base-infra/uat/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-project_name = "carro"
-vpc_name     = "primary-vpc"
-environment  = "dev"
-region       = "us-east-1"
